7c82c781d1d71d89054750f316f40be905765ca3
shaun standing <shaunstanding88@gmail.com>
2022-06-07 16:19:39 +0100

    first commit

---
+++ level_up.py
@@ -0,0 +1,115 @@
+# print(int(5.4))
+# print(int("54"))
+
+# print(float(54))
+# print(float("54"))
+
+# print(str(54))
+# print(str(54.0))
+
+# #truthy falsy. anything without a value is falsy!
+# variable_one = 0
+
+# if variable_one:
+#     print("this is truthy")
+# else:
+#     print("this is falsy")
+
+
+# print("What is your name?")
+# name = input()
+
+# if name:
+#     print(f"Hello {name}, how are you?")
+# else:
+#     print("you did not give me your name!")
+
+#Not operator !=
+
+# list = ["J","Q","K","A"]
+
+# if "A" not in list:
+#     print("The Ace is missing!")
+# elif "2"not in list and "3" not in list:
+#     print("This list has positive equity!")
+
+# try / except 
+
+# def add_up():
+#     num1 = input("What is the first number you'd like to add up? \n")
+#     num2 = input("What is the second number you would like to add up? \n")
+
+#     try:
+#         print(f"{num1} + {num2} is {(int(num1) + int(num2))}!")
+#     except: print("\n Error")
+#     print("******************\n")
+#     add_up()
+
+# add_up()
+
+
+#Scope - where something exists. global/local
+
+# light = False
+
+# def light_switch():
+#     global light
+#     if light:
+#         print("Whoa! It\'s bright in here")
+#         light = False
+#     else:
+#         print("Who turned out the lights?")
+#         light = True
+
+# light_switch()
+# light_switch()
+
+# list/tuples
+# even_nums = [2, 4, 6, 8, 10] #Mutable list
+# odd_nums = (1, 3, 5, 7, 9) #Immutable tuple
+
+# slicing notation 
+# my_list[start:stop]
+# my_list[start:]
+# my_list[:stop]
+# my_list[:]
+
+# While True
+
+# while True:
+#     print("laptop is dying laa!")
+
+#while loops break/continue
+
+# loop = True
+# name = "Admin"
+
+# while loop:
+#     if name == "Admin":
+#         print("Hello Admin")
+#         break
+#     else:
+#         print(f"welcome {name}!")
+#         continue
+
+#Activity 1
+
+# def program():
+#     user_input = input("Please enter a number!:\n")
+#     try:
+#         user_input =  int(user_input)
+#         print(f"{user_input}",type(user_input))
+    
+#     except:
+#         print("Try again!")
+#         program()
+
+# program()
+
+
+
+
+
+
+
+
---
+++ recap.py
@@ -0,0 +1,31 @@
+# activity(1)
+
+# welcome = "Welcome to Code Nation"
+
+# welcome = len(welcome)
+
+# def odd_even():
+#     if welcome % 2==0:
+#         print(f"{welcome}  is an even number")
+#     else:
+#         print(f"{welcome} is an odd number")
+    
+
+# odd_even()
+
+
+# activity(2)
+
+# alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+# for x in alphabet:
+#     print(x)
+
+# def get_num():
+#     num = input("Enter a number between 1-26:")
+#     num = int(num) - 1
+#     print(alphabet[num].upper())
+
+# get_num()
+
+
