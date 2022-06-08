2f320da7a3a9000f3336e13fcf2b68e4ca4b52d2
shaun standing <shaunstanding88@gmail.com>
2022-06-08 14:32:59 +0100

    update

---
+++ dictionaries.py
@@ -0,0 +1,62 @@
+
+
+
+# dog_1 = ["Max","Brown","Placid"]
+
+#dog_2 = {"name": "Max", "color":"Brown", "personality":"Placid"}
+
+# print(dog_2["name"])
+
+# for x in dog_2.values():
+#     print(x)
+# for x in dog_2.keys():
+#     print(list(x))
+# for x in dog_2.items():
+#     print(list(x))
+
+# dog_2["color"] = "Black"
+
+# print(dog_2.values())
+# print(dog_2.keys())
+# print(dog_2.items())
+# print(dog_2.get())
+
+
+
+
+
+#complex_dictionary = {"name":"clover", "age": 3, "amazing":True, "species": "budgie" }
+# {"name": "belle", "age":2,"playful":True, "species":"budgie"}
+
+# for x in complex_dictionary.values():
+#     print(x)
+
+# complex_dictionary["name"] = "belle"
+# complex_dictionary["age"] = 2
+# new_key = "playful"
+# old_key = "amazing"
+# complex_dictionary[new_key] = complex_dictionary.pop(old_key)
+
+# for x in complex_dictionary.keys():
+#     print(x)
+# dog_2.setdefault("hungry", True)
+
+
+# count = {"United Kingdom": "London", "France":"Paris", "Germany":"Berlin", "Spain":"Madrid","italy":"Rome"}
+
+# count.setdefault("Hungary","Budapest")
+# count.setdefault("China","Beijing")
+
+# print(count)
+# print(count.items()) #I prefer this method because of the way it presents the data and that it gives me all data from the dictionary. 
+# print(count.values())
+# print(count.keys())
+
+
+
+
+fav_songs = [{"Artist":""}, {"Song":""}, {"Genre":""}, {"Year":""}]
+
+fav_songs[0] ["Artist"] = "Nirvana"
+
+print(fav_songs)
