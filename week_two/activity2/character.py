e3a3bfe2da49e2069a6ce3e259bb0bcb767b029c
shaun standing <shaunstanding88@gmail.com>
2022-06-13 17:08:46 +0100

    update

---
+++ week_two/activity2/character.py
@@ -0,0 +1,18 @@
+
+class Character():
+    def __init__(self):
+        self.real_name = None
+        self.hero_name = None
+
+    def super_power(self):
+        self.super_power 
+        
+
+    def set_power(self, super_power):
+        self.power = super_power
+
+    def get_power(self):
+        return self.power
+
+
+
